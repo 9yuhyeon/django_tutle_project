e57c2b6e1f2fc31c95c8577ff252237223eb31fe
9yuhyeon <kkh2742@gmail.com>
2022-10-13 21:43:03 +0900

    make user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django import http
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import User
+from django.contrib.auth import authenticate, login as loginsession
 
 # Create your views here.
 def signup(request):
@@ -14,8 +15,34 @@ def signup(request):
         
         if password == password2:       
             User.objects.create_user(username=username, password=password)
-            return redirect('/user')
+            return redirect('users:login')
         else:
             return HttpResponse('로그인 실패')
 
-            
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            loginsession(request, user)
+            return redirect('users:user')
+        else:
+            return HttpResponse('로그인 실패!')
+
+
+def user(request):
+    return HttpResponse(request.user)
+
+
+def profile(request, username ):
+    user = User.objects.get(username=username)
+    context = {
+        'user':user
+    }
+    return  render(request, 'profile.html', context)

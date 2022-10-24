8c41d163ac7baac37a1e71072c89812cf22d80e1
9yuhyeon <kkh2742@gmail.com>
2022-10-24 17:38:23 +0900

    Add article(write,detail,update,delete) function in community app

--- community/views.py
+++ community/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from community.models import Article
+from django.shortcuts import get_object_or_404
 # Create your views here.
+def index(request):
+    articles = Article.objects.all().order_by('-created_at')
+    context = {
+        'articles':articles
+    }
+
+    return render(request, 'index.html', context)
+
+
+def create_article(request):
+    if request.method == 'GET':
+        return render(request, 'create_article.html')
+    
+    elif request.method == 'POST':
+        title = request.POST.get('title','')
+        content = request.POST.get('content','')
+        user = request.user
+        Article.objects.create(title=title, content=content, user=user)
+        return redirect('community:index')
+
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+
+    context = {
+        'article':article
+    }
+    return render(request, 'article_detail.html', context)
+
+
+def update_article(request, article_id):
+    article = Article.objects.get(id=article_id)
+    if request.user != article.user:
+        return HttpResponse('본인이 작성한 게시물만 수정/삭제 할 수 있습니다.')
+
+    if request.method == 'GET':
+        context = {
+            'article':article
+        }
+        return render(request, 'update_article.html', context)
+    
+    elif request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+
+        return redirect('community:article_detail', article_id)
+
+def delete_article(request,article_id):
+    if request.method == 'POST':
+        article = Article.objects.get(id=article_id)
+        
+        if request.user != article.user:
+            return HttpResponse('본인이 작성한 게시물만 수정/삭제 할 수 있습니다.')
+        
+        article.delete()
+        
+        return redirect('community:index')
